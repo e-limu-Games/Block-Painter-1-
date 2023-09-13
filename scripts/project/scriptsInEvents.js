e1089cce4485df8c3d215975cfa40d548f1363b5
Jean Confident Irene Niyizibyose <game@e-limu.org>
2023-09-13 11:01:23 +0200

    new

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -35,11 +35,9 @@ const scriptsInEvents = {
 					runtime.globalVars.Level = obj.payload.BP_Level;
 					runtime.globalVars.Money = obj.payload.BP_Money;
 					
-				/*	runtime.globalVars.OwnedBuildings_Data = JSON.stringify(obj.payload.BP_OwnedBuildings_Data);
-					runtime.objects.OwnedBuildings= JSON.parse(runtime.globalVars.OwnedBuildings_Data);  */
+				runtime.globalVars.OwnedBuildings_Data = obj.payload.BP_OwnedBuildings_Data;
 						
 					runtime.globalVars.BuildedBuildings_Data = obj.payload.BP_BuildedBuildings;
-					//runtime.objects.BuildedBuildings= JSON.stringify(obj.payload.BP_BuildedBuildings) ;
 				
 					if (typeof obj.payload.BP_Level === 'undefined'){
 				runtime.globalVars.Level = 1;
@@ -49,11 +47,11 @@ const scriptsInEvents = {
 				runtime.globalVars.Money = 0;
 				}
 				
-			/*	if (typeof obj.payload.BP_OwnedBuildings_Data === 'undefined'){
-			 runtime.globalVars.OwnedBuildings_Data = JSON.stringify({"c2array":true,"size":[1,1,1],"data":[[[7]]]});}
+				if (typeof obj.payload.BP_OwnedBuildings_Data === 'undefined'){
+			 runtime.globalVars.OwnedBuildings_Data = ({"c2array":true,"size":[1,1,1],"data":[[[7]]]});}
 				 
 					if (typeof obj.payload.BP_BuildedBuildings === 'undefined'){
-			 runtime.globalVars.BuildedBuildings_Data = JSON.stringify({"c2array":true,"size":[1,3,1],"data":[[["22"],[2],[0]]]});}  */
+			 runtime.globalVars.BuildedBuildings_Data = ({"c2array":true,"size":[1,3,1],"data":[[["22"],[2],[0]]]});}  
 				
 					}
 			}
